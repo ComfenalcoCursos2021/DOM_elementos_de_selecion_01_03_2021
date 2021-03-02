e72053a8eb6936a465f51ba78d2424590cdc30d2
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-03-01 21:11:12 -0500

    01_03_2021

---
+++ Adrian_Velasquez_monchom/main.js
@@ -0,0 +1,68 @@
+
+
+
+window.onload = function() {
+    const nodo = document.getElementById("Caja");
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 10000; i++) {
+        const nodoP = document.createElement("script");
+        const texto = document.createTextNode(`console.log('Hola ${i}');`);
+        nodoP.appendChild(texto);
+        fragment.appendChild(nodoP);
+    }
+    nodo.append(fragment);
+
+};
+window.onload = function() {
+    const nodo = document.getElementById("Caja");
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 10000; i++) {
+        const nodoP = document.createElement("script");
+        const texto = document.createTextNode(`console.log('Hola ${i}');`);
+        nodoP.appendChild(texto);
+        fragment.appendChild(nodoP);
+    }
+    nodo.append(fragment);
+
+};
+window.onload = function() {
+    const nodo = document.getElementById("Caja");
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 10000; i++) {
+        const nodoP = document.createElement("script");
+        const texto = document.createTextNode(`console.log('Hola ${i}');`);
+        nodoP.appendChild(texto);
+        fragment.appendChild(nodoP);
+    }
+    nodo.append(fragment);
+
+};
+window.onload = function() {
+    const nodo = document.getElementById("Caja");
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 10000; i++) {
+        const nodoP = document.createElement("script");
+        const texto = document.createTextNode(`console.log('Hola ${i}');`);
+        nodoP.appendChild(texto);
+        fragment.appendChild(nodoP);
+    }
+    nodo.append(fragment);
+
+};
+window.onload = function() {
+    const nodo = document.getElementById("Caja");
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 10000; i++) {
+        const nodoP = document.createElement("script");
+        const texto = document.createTextNode(`console.log('Hola ${i}');`);
+        nodoP.appendChild(texto);
+        fragment.appendChild(nodoP);
+    }
+    nodo.append(fragment);
+
+};
